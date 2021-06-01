cf0f353dce46521c1610e2307424d7936663a783
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-06-01 10:04:31 +0530

    docs:initialized folder structure for frontend

---
+++ frontend/src/Screens/NavBar/Index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './Nav'
+import "./App.css"
+
+function Index() {
+    return (
+        <>
+            <Nav/>
+        </>
+    )
+}
+
+export default Index
---
+++ frontend/src/Screens/NavBar/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./App.css"
+function Nav() {
+    return (
+        <div>
+            Nav bar
+        </div>
+    )
+}
+
+export default Nav

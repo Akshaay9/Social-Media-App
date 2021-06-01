852cd9c81b99ef81a567d7db5042c26723a6bd6e
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-06-01 16:52:01 +0530

    feat & style:added side nav with user card:

--- frontend/src/Screens/LeftSideBar/LeftSideBar.js
+++ frontend/src/Screens/LeftSideBar/LeftSideBar.js
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React from "react";
 function LeftSideBar() {
-    return (
-        <div>
-            {/* lorem200 */}
-        </div>
-    )
+  return (
+    <div className="left_nav_bar">
+      <ul>
+        <li>
+          <i class="fas fa-home"></i>
+          News Feed
+        </li>
+        <li>
+          <i class="fas fa-user-circle"></i>
+          Profile
+        </li>
+        <li>
+          <i class="fas fa-users"></i>
+          People
+        </li>
+        <li>
+          <i class="fas fa-cog"></i>
+          Seetings
+        </li>
+        <li>
+          <i class="fas fa-sign-out-alt"></i>
+          Log-Out
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default LeftSideBar
+export default LeftSideBar;

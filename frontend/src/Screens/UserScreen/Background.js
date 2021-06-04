cbeedf693c03d3f06c1e44a85294f23e5dab0f8c
Akshaay9 <72427030+Akshaay9@users.noreply.github.com>
2021-06-04 13:35:03 +0530

    feat:made users page mobile responsive

--- frontend/src/Screens/UserScreen/Background.js
+++ frontend/src/Screens/UserScreen/Background.js
@@ -19,22 +19,52 @@ const useStyles = makeStyles((theme) => ({
     position: "absolute",
     top: "73%",
     left: "45%",
-    border:"4px solid #3a3b3c"
+    border: "4px solid #3a3b3c",
+  },
+}));
+const useStyles2 = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  large: {
+    width: theme.spacing(13),
+    height: theme.spacing(13),
+    display: "block",
+    margin: "auto",
+    position: "absolute",
+    top: "82%",
+    left: "33%",
+    border: "4px solid #3a3b3c",
   },
 }));
 function Background() {
   const classes = useStyles();
+  const classes2 = useStyles2();
   return (
     <div>
       <div className="background-image-container">
         <div className="background-image">
           <img src="https://images.unsplash.com/reserve/HgZuGu3gSD6db21T3lxm_San%20Zenone.jpg?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fG5hdHVyZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
 
+          <div className="mobile-hide">
+            <Avatar
+              alt="Remy Sharp"
+              src={postData[0].userID?.profileImage}
+              className={classes.large}
+            />
+             <i class="fas fa-camera user-avatar-camera"></i>
+          </div>
+          <div className="desktop-hide">
           <Avatar
             alt="Remy Sharp"
             src={postData[0].userID?.profileImage}
-            className={classes.large}
-          />
+            className={classes2.large}
+            />
+             <i class="fas fa-camera user-avatar-camera"></i>
+        </div>
         </div>
         <div className="background-user-info">
           <h1>Kai</h1>
@@ -46,6 +76,11 @@ function Background() {
             <p>12 Following</p>
             <p>1 Followers</p>
           </div>
+        </div>
+       
+        <div className="update-user-bg-image">
+          <i class="fas fa-camera user-bg-camera"></i>
+          <p>Edit cover photo</p>
         </div>
       </div>
     </div>

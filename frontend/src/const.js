6267a3de48fe4e5ea77ebf511b7ffd3a949ee4c9
Akshaay9 <nair.akshay9898@gmail.com>
2022-10-23 14:38:47 +0530

    BE to Render.io

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,1 +1,2 @@
-export const BE_URL = "https://socialmediabe.akshaynair5.repl.co";
+// export const BE_URL = "https://socialmediabe.akshaynair5.repl.co"; <-repl
+export const BE_URL = "https://fit-shark-socialmedia-be-v1.onrender.com";

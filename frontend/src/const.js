64f3d658ac2f7b3a8fff547647fadd4748721ea3
Akshaay9 <nair.akshay9898@gmail.com>
2025-02-01 12:55:55 +0530

    DB migration

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,2 +1,2 @@
-export const BE_URL = "http://localhost:5000";
+export const BE_URL = "https://social-media-app-bnc9.onrender.com";
 // export const BE_URL = "https://fit-shark-socialmedia-be-v1.onrender.com";
